c8a28a22054ce8942f0e6ce546391b38f19f0c7c
Parker Lippstock <parkman202@gmail.com>
2024-04-17 21:09:25 -0500

    fix flashbang again

--- inktrinket_v2/main.js
+++ inktrinket_v2/main.js
@@ -1,20 +1,15 @@
-// Get user color scheme preferences and set theme first
-// window.onload = function getColorScheme(){
-// }
+// Get user theme settings saved by the site (if they exist)
+var theme = localStorage.getItem("theme");
 
+// Set the user's color scheme
 function getColorScheme(){
-    var theme = localStorage.getItem("theme");
     if (theme !== null){ // If user has visited prior
         document.documentElement.setAttribute("data-theme", theme);
-        if(theme === "dark"){ // Check checkbox if they prefer dark mode
-            document.getElementById("night-light").checked = true
-        }
         return;
     }
     if (prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches){ // If user has prefers dark mode
         document.documentElement.setAttribute("data-theme", "dark");
         localStorage.setItem("theme", "dark");
-        document.getElementById("night-light").checked = true
         return;
     }
     // Define light theme and set otherwise
@@ -22,16 +17,23 @@ function getColorScheme(){
     localStorage.setItem("theme", "light");
 }
 
-getColorScheme()
+getColorScheme() // Run before page draws
 
-// Toggle dark mode via the night light
-var checkbox = document.getElementById("night-light");
-checkbox.addEventListener("change", function() {
-    if (this.checked) {
-        document.documentElement.setAttribute("data-theme", "dark");
-        localStorage.setItem("theme", "dark");
-    } else {
-        document.documentElement.setAttribute("data-theme", "light");
-        localStorage.setItem("theme", "light");
+// On page load, check the checkbox if the user prefers dark mode and then add a listener for the checkbox
+window.onload = function applyCheck(){
+    if(theme === "dark"){
+        document.getElementById("night-light").checked = true
     }
-});
+
+    // Toggle dark mode via the night light
+    var checkbox = document.getElementById("night-light");
+    checkbox.addEventListener("change", function() {
+        if (this.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+        }
+    });
+}
