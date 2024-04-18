63a073b2375453dbf721cb7cfb98d92af71492da
Parker Lippstock <parkman202@gmail.com>
2024-04-17 20:56:41 -0500

    no more flashbang

--- inktrinket_v2/main.js
+++ inktrinket_v2/main.js
@@ -1,18 +1,28 @@
 // Get user color scheme preferences and set theme first
-window.onload = function getColorScheme(){
+// window.onload = function getColorScheme(){
+// }
+
+function getColorScheme(){
     var theme = localStorage.getItem("theme");
-    if (theme !== null){
+    if (theme !== null){ // If user has visited prior
         document.documentElement.setAttribute("data-theme", theme);
+        if(theme === "dark"){ // Check checkbox if they prefer dark mode
+            document.getElementById("night-light").checked = true
+        }
         return;
     }
-    if (prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches){
+    if (prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches){ // If user has prefers dark mode
         document.documentElement.setAttribute("data-theme", "dark");
         localStorage.setItem("theme", "dark");
+        document.getElementById("night-light").checked = true
         return;
     }
+    // Define light theme and set otherwise
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "light");
 }
+
+getColorScheme()
 
 // Toggle dark mode via the night light
 var checkbox = document.getElementById("night-light");
