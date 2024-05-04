1dac39c4b2fe64ddf3bfeff8399248155d360998
Parker Lippstock <parkman202@gmail.com>
2024-05-04 02:34:17 -0500

    optimizations to night light, groundwork for night light notif and toast

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Get user theme settings saved by the site (if they exist)
 var theme = localStorage.getItem("theme");
-var image_other = new Image();
 
 // Set the user's color scheme
 function getColorScheme(){
@@ -21,6 +20,7 @@ getColorScheme() // Run before page draws
 
 // On page load, check the checkbox if the user prefers dark mode and then add a listener for the checkbox
 window.onload = function applyCheck(){
+    document.documentElement.style.setProperty("--night-light-url", `url(/img/night_light/nl_${theme}.png)`);
 
     var other = "dark";
     switch(theme){
@@ -32,13 +32,10 @@ window.onload = function applyCheck(){
             break;
     }
 
-    image_other.src = `/img/night_light/nl_${other}.png`;
-
     // Toggle dark mode via the night light
     var checkbox = document.getElementById("night-light");
     
     var v_checkbox = document.getElementById("night-light-label");
-    v_checkbox.style.backgroundImage = `url(/img/night_light/nl_${theme}.png)`;
 
     v_checkbox.addEventListener("mousedown", function() {
         this.classList.add("pulldown");
@@ -53,6 +50,6 @@ window.onload = function applyCheck(){
         }
         document.documentElement.setAttribute("data-theme", theme);
         localStorage.setItem("theme", theme);
-        v_checkbox.style.backgroundImage = `url(/img/night_light/nl_${theme}.png)`;
+        document.documentElement.style.setProperty("--night-light-url", `url(/img/night_light/nl_${theme}.png)`);
     });
 }
